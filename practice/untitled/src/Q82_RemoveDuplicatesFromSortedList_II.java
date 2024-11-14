523b477b7123332d14c3f7e53025375244b70dd5
Tejasvi Rozukurthi <rao.tejasvi@gmail.com>
2024-11-13 22:09:55 -0500

    Linked List Problems - 13Nov2024

---
+++ practice/untitled/src/Q82_RemoveDuplicatesFromSortedList_II.java
@@ -0,0 +1,40 @@
+public class Q82_RemoveDuplicatesFromSortedList_II {
+    public static void main(String[] args) {
+        SinglyLinkedList sl1 = new SinglyLinkedList();
+        sl1.insert(1);
+        sl1.insert(1);
+        sl1.insert(2);
+        sl1.insert(3);
+        sl1.insert(4);
+        sl1.display(sl1.head);
+
+        Q82_RemoveDuplicatesFromSortedList_II q82 = new Q82_RemoveDuplicatesFromSortedList_II();
+        SinglyLinkedList.ListNode node = q82.deleteDuplicates_1(sl1.head);
+        sl1.display(node);
+    }
+
+    public SinglyLinkedList.ListNode deleteDuplicates(SinglyLinkedList.ListNode head) {
+        // Sentinel
+        SinglyLinkedList.ListNode sentinel = new SinglyLinkedList.ListNode(0, head);
+        SinglyLinkedList.ListNode pred = sentinel;
+
+        while (head != null) {
+            if (head.next != null && head.val == head.next.val) {
+                // Move till the end of the duplicates sublist
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;
+                }
+
+                // Skip all duplicates
+                pred.next = head.next;
+                // otherwise, move predecessor
+            } else {
+                pred = pred.next;
+            }
+
+            // move forward
+            head = head.next;
+        }
+        return sentinel.next;
+    }
+}
