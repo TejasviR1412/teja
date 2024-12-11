c3371951217ac670bccc440bc12b7753d5e6e02b
Tejasvi Rozukurthi <rao.tejasvi@gmail.com>
2024-12-11 11:45:46 -0500

    Binary search problems - 10-Dec-2024

---
+++ practice/untitled/src/Q1891_CuttingRibbons.java
@@ -0,0 +1,17 @@
+public class Q1891_CuttingRibbons {
+    public static void main(String[] args) {
+        int[] ribbons1 = {9,7,5};
+        int k1 = 3;
+
+        int[] ribbons2 = {7,5,9};
+        int k2 = 4;
+
+        int[] ribbons3 = {5,7,9};
+        int k3 = 22;
+    }
+
+    public int maxLength(int[] ribbons, int k) {
+
+        return 0;
+    }
+}
